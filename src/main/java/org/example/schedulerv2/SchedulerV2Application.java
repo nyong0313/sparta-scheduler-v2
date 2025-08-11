1eab81a26355c2896b3f40ae9e1efab08538ddd9
nyong0313 <ghdyd6625@naver.com>
2025-08-11 10:21:13 +0900

    chore: DB 설정 추가

--- src/main/java/org/example/schedulerv2/SchedulerV2Application.java
+++ src/main/java/org/example/schedulerv2/SchedulerV2Application.java
@@ -2,8 +2,12 @@ package org.example.schedulerv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@PropertySource("classpath:secrets.properties")
 @SpringBootApplication
+@EnableJpaAuditing
 public class SchedulerV2Application {
 
     public static void main(String[] args) {

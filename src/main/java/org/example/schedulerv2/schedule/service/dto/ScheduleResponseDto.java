917806831df752cb6e9d1bd9f8933b86d24ade06
nyong0313 <ghdyd6625@naver.com>
2025-08-12 18:38:53 +0900

    fix: 유저 응답 DTO 수정 (id 필드 추가)

--- src/main/java/org/example/schedulerv2/schedule/service/dto/ScheduleResponseDto.java
+++ src/main/java/org/example/schedulerv2/schedule/service/dto/ScheduleResponseDto.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 @Getter
 public class ScheduleResponseDto {
 
+    private final Long id;
     private final String writerName;
     private final String title;
     private final String contents;
@@ -15,11 +16,13 @@ public class ScheduleResponseDto {
     private final LocalDateTime updatedAt;
 
     public ScheduleResponseDto(
+            Long id,
             String writerName,
             String title,
             String contents,
             LocalDateTime createdAt,
             LocalDateTime updatedAt ) {
+        this.id = id;
         this.writerName = writerName;
         this.title = title;
         this.contents = contents;
@@ -29,6 +32,7 @@ public class ScheduleResponseDto {
 
     public static ScheduleResponseDto from(Schedule schedule) {
         return new ScheduleResponseDto(
+                schedule.getId(),
                 schedule.getWriterName(),
                 schedule.getTitle(),
                 schedule.getContents(),

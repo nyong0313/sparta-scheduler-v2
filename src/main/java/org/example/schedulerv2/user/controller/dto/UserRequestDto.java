36582c2b836eaccd0e2b57b039307402b5d47209
nyong0313 <ghdyd6625@naver.com>
2025-08-16 21:49:11 +0900

    fix: 유저 이름 길이 제한 수정

--- src/main/java/org/example/schedulerv2/user/controller/dto/UserRequestDto.java
+++ src/main/java/org/example/schedulerv2/user/controller/dto/UserRequestDto.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 public class UserRequestDto {
 
     @NotBlank(message = "사용자명은 필수입니다.")
-    @Size(min = 4, max = 10, message = "사용자명은 4자 이상 10자 이하이어야 합니다.")
+    @Size(min = 2, max = 10, message = "사용자명은 4자 이상 10자 이하이어야 합니다.")
     private String username;
     @NotBlank(message = "이메일은 필수입니다.")
     @Email(message = "유효한 이메일 형식이 아닙니다.")
--- src/main/java/org/example/schedulerv2/user/entity/User.java
+++ src/main/java/org/example/schedulerv2/user/entity/User.java
@@ -14,7 +14,7 @@ public class User extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true, length = 10)
+    @Column(nullable = false, unique = true, length = 30)
     private String username;
     @Column(nullable = false, unique = true)
     private String email;
